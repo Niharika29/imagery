0aeba021f5658e42436c9c90ef426508190f06cd
Niharika Kohli <niharikakohli29@gmail.com>
2018-05-20 11:41:56 +0200

    CORS things!!

--- public_html/api.php
+++ public_html/api.php
@@ -5,12 +5,13 @@ require_once '../vendor/autoload.php';
 
 header( 'Access-Control-Allow-Origin: *' );
 
+
 // Get the image from the API call
 $image = $_GET['image'] ?: 'test';
-$jsonp_callback = isset( $_GET['callback'] ) ? $_GET['callback'] : null;
+// $jsonp_callback = isset( $_GET['callback'] ) ? $_GET['callback'] : null;
 
 // Create a new Imagery instance and give it the image
 $imagery = new Imagery( $apiKey );
 $data = json_encode( $imagery->main( $image ) );
 
-print $jsonp_callback ? $jsonp_callback . '(' . $data . ')' : $data;
+print 'displayResult(\'' . $data . '\')';
